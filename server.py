d9e4e4e41d1558c8a494a66e8f7d59e7a5b87986
xdosta51 <48760239+xdosta51@users.noreply.github.com>
2020-03-02 12:37:49 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
 import socket
-import sys
 import re
+import sys
+
+if len(sys.argv) is not 2: # test na pocet argumentu
+    print("Spatne argumenty.")
+    sys.exit(1)
+port_arg = int(sys.argv[1])
+if not isinstance(port_arg,int) or port_arg < 1024 or port_arg > 65535: # test na port_arg
+    print("Spatne cislo portu nebo spatny format cisla portu")
+    sys.exit(1)
+
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to the port
-server_address = ('localhost',9000)
+# Bind the socket to the port_arg
+server_address = ('localhost', port_arg)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(server_address)
 
 # Listen for incoming connections
@@ -13,17 +23,30 @@ sock.listen(1)
 
 while True:
     # Wait for a connection
-    connection = sock.accept()
+    connection = None
     try:
+        connection, client_address = sock.accept()
         while True:
-            data = connection.recv(1024)
+            data = connection.recv(512)
             datanew = data.decode("utf-8")
-            itisok = re.search('/resolve(.+?)', datanew)
-            itisturbo = re.search('/dns-query(.+?)', datanew)
+            gethttp = datanew
+            gethttp = gethttp.split('\n', 1)
+            lajna = gethttp[0]
+            words = lajna.split()
+            if words[-1] == 'HTTP/1.1' or words[-1] == 'HTTP/1.0':
+                version = words[-1] + ' '
+            else:
+                messageforclient = version + '500 Internal Server Error\r\n\r\n'
+                messageforclient = messageforclient.encode()
+                connection.sendall(messageforclient)
+                break
+            
+            itisok = re.search('/resolve\\?', datanew)
+            itisturbo = re.search('/dns-query\s', datanew)
             if not (itisok) and not (itisturbo):
-                hovno = '400 Bad Request\r\n\r\n'
-                hovno = hovno.encode()
-                connection.sendall(hovno)
+                messageforclient = version + '400 Bad Request\r\n\r\n'
+                messageforclient = messageforclient.encode()
+                connection.sendall(messageforclient)
                 break
             itisget = re.search('GET (.+?) HTTP', datanew)
             itispost = re.search('POST (.+?) HTTP', datanew)
@@ -32,14 +55,15 @@ while True:
             elif itispost:
                 itispost = itispost.group(1)
             else: 
-                hovno = '400 Bad Request\r\n\r\n'
-                hovno = hovno.encode()
-                connection.sendall(hovno)
+                messageforclient = version + '405 Method Not Allowed\r\n\r\n'
+                messageforclient = messageforclient.encode()
+                connection.sendall(messageforclient)
                 break
             if itisget:
                 
                 m = re.search('name=(.+?)&', datanew)
                 ipadr = re.search('name=(.+?) HTTP', datanew)
+                
                 if m:
                     found = m.group(1)
                     n = re.search('type=(.+?) ', datanew)
@@ -48,34 +72,39 @@ while True:
                     foundtype = 'PTR'
                     found = ipadr.group(1)
                 else:
-                    print('chyba')
+                    messageforclient = version + '400 Bad Request\r\n\r\n'
+                    messageforclient = messageforclient.encode()
+                    connection.sendall(messageforclient)
+                    break
                 if foundtype == 'A':
+
                     try:
                         socket.gethostbyname(found)
-                        turbink = found + ':' + foundtype + '=' + socket.gethostbyname(found) + '\n'
+                        zpravaproklienta = found + ':' + foundtype + '=' + socket.gethostbyname(found) + '\n'
                     except: 
-                        turbink = '404 Not Found.\r\n\r\n'
+                        zpravaproklienta = version + '404 Not Found.\r\n\r\n'
                         
                 elif foundtype == 'PTR':
+                    
                     try:
-                        socket.gethostbyname(found)
-                        picakurva = socket.gethostbyaddr(found)
-                        turbink = found + ':' + foundtype + '=' + picakurva[0] + '\n'
+                        socket.gethostbyaddr(found)
+                        getnamee = socket.gethostbyaddr(found)
+                        zpravaproklienta = found + ':' + foundtype + '=' + getnamee[0] + '\n'
                     except: 
-                        turbink = '404 Not Found.\r\n\r\n'
+                        zpravaproklienta = version + '404 Not Found.\r\n\r\n'
                         
-                if not turbink == '404 Not Found.\r\n\r\n':
-                    hovno = 'HTTP/1.1 200 OK\r\n\r\n' + turbink
+                if not zpravaproklienta == version + '404 Not Found.\r\n\r\n':
+                    messageforclient = version + '200 OK\r\n\r\n' + zpravaproklienta
                 else:
-                    hovno = turbink
-                hovno = hovno.encode()
+                    messageforclient = zpravaproklienta
+                messageforclient = messageforclient.encode()
             elif itispost:
         
                 groupof = datanew.split('\r\n\r\n', 1)
                 groupof = groupof[1].split('\n')
                 counter = len(groupof)
-                hovno = ''
-                
+                messageforclient = ''
+                hlavicka = ''
                 for addr in groupof:
                     
                     typeof = re.search(':(.+?)', addr)
@@ -83,43 +112,55 @@ while True:
                         typeof = typeof.group(1)
                     else:
                         continue
-                    kurvicka = addr + '='
-                
-                    jebicka = re.search('(.+?):PTR', addr)
-                    if jebicka:
-                        addr = jebicka.group(1)
+                    messagetok = addr + '='
+                    
+                    iptosolve = re.search('(.+?):PTR$', addr)
+                    if iptosolve:
+                        addr = iptosolve.group(1)
                         typeof = 'P'
                     else :
-                        jebicka = re.search('(.+?):A', addr)
-                        if jebicka:
-                            addr = jebicka.group(1)
+                        iptosolve = re.search('(.+?):A$', addr)
+                        if iptosolve:
+                            addr = iptosolve.group(1)
                             typeof = 'A'
                         else:
-                            break
+                            hlavicka = version + '400 Bad Request\r\n\r\n'
+                            continue
                         
                     if typeof == 'A':
-                        hovno = hovno + kurvicka  + socket.gethostbyname(addr) + '\n'
-                        
+                        try:
+                            messageforclient = messageforclient + messagetok  + socket.gethostbyname(addr) + '\n'
+                        except:
+                            hlavicka = version + '404 Not Found.\r\n\r\n'
                     elif typeof == 'P':
-                        picakurva = socket.gethostbyaddr(addr)
-                        hovno = hovno + kurvicka + picakurva[0] +'\n'
+                        try:
+                            getnamee = socket.gethostbyaddr(addr)
+                            messageforclient = messageforclient + messagetok + getnamee[0] +'\n'
+                        except:
+                            hlavicka = version + '404 Not Found.\r\n\r\n'
                         
-                    else:
-                        print('zmrde')
-                hovno = 'HTTP/1.1 200 OK\r\n\r\n' + hovno
-                hovno = hovno.encode()
+                    else:   
+                        continue
+                if not hlavicka:
+                    hlavicka = version + '200 OK\r\n\r\n'
+                messageforclient = hlavicka + messageforclient
+                messageforclient = messageforclient.encode()
             else :
-                hovno = '400 Bad Request\r\n\r\n'
-                hovno = hovno.encode()
-                connection.sendall(hovno)
+                messageforclient = version + '400 Bad Request\r\n\r\n'
+                messageforclient = messageforclient.encode()
+                connection.sendall(messageforclient)
                 break
             if data:
-                connection.sendall(hovno)
+                connection.sendall(messageforclient)
                 break
             else:
                 
                 break
-          
-    finally:
-        # Clean up the connection
-        connection.close()
+    except KeyboardInterrupt:
+        try:
+            if connection:
+                connection.close()
+        except: pass
+        break
+sock.shutdown
+sock.close()
